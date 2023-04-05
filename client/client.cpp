5c9f4ac8b541d74292064e31a89a9cd847a443f7
fishmint <12210470+fishmint@user.noreply.gitee.com>
2023-04-05 09:14:06 +0800

    添加项目文件。

---
+++ client/client.cpp
@@ -0,0 +1,88 @@
+﻿#include "hv/requests.h"
+#include "hv/axios.h"
+using namespace hv;
+#include "hv/hthread.h" // import hv_gettid
+
+
+static std::string Recv(HttpClient* cli, std::string hash, std::string phone) {
+    HttpRequest req;
+    req.method = HTTP_GET;
+    req.url = "http://127.0.0.1:8080/recv";
+    req.SetParam("hash", hash);
+    req.SetParam("phone", phone);
+
+    req.headers["Connection"] = "keep-alive";
+    req.timeout = 60;
+    HttpResponse resp;
+    int ret = cli->send(&req, &resp);
+    if (ret != 0) {
+        return "";
+    }
+    else {
+        printf("%d %s\r\n", resp.status_code, resp.status_message());
+        return resp.body.c_str();
+    }
+    return "";
+}
+static std::string Get_Recv(HttpClient* cli, std::string hash) {
+    HttpRequest req;
+    req.method = HTTP_GET;
+    req.url = "http://127.0.0.1:8080/can_recv";
+    req.SetParam("hash", hash);
+    req.headers["Connection"] = "keep-alive";
+    req.timeout = 60;
+    HttpResponse resp;
+    int ret = cli->send(&req, &resp);
+    if (ret != 0) {
+        return "";
+    }
+    else {
+        printf("%d %s\r\n", resp.status_code, resp.status_message());
+        return resp.body.c_str();
+    }
+    return "";
+}
+static std::string login(HttpClient* cli, std::string token) {
+    HttpRequest req;
+    req.method = HTTP_GET;
+    req.url = "http://127.0.0.1:8080/login";
+    req.SetParam("token", token);
+    req.headers["Connection"] = "keep-alive";
+    req.timeout = 60;
+    HttpResponse resp;
+    int ret = cli->send(&req, &resp);
+    if (ret != 0) {
+        printf("request failed!\n");
+        return "";
+    }
+    else {
+        printf("%d %s\r\n", resp.status_code, resp.status_message());
+        return resp.body.c_str();
+    }
+    return "";
+}
+int main(int argc, char* argv[]) {
+    std::string token = argv[1];
+    int req_cnt = 2;
+    HttpClient sync_client;
+    HttpClient async_client;
+    int resp_cnt = 0;
+    std::string hash=login(&sync_client, token);
+    std::cout << hash << std::endl;
+
+    auto s=    hash.find_last_of("\"");
+    std::string hash22 = hash.substr(hash.find(":")+2, s - hash.find(":")-2);
+
+    getchar();
+    for (int i = 0; i < req_cnt; ++i) {
+        std::string ccs=Get_Recv(&sync_client, hash22);
+        std::cout << ccs << std::endl << std::endl;
+        std::string ccc = Recv(&sync_client, hash22, "1111");
+        std::cout << ccc << std::endl << std::endl;
+    }
+
+    while (resp_cnt < req_cnt) hv_delay(100);
+    printf("finished!\n");
+    getchar();
+    return 0;
+}
---
+++ server_redis/redis_my.h
@@ -0,0 +1,112 @@
+#ifndef _REDIS_H_
+#define _REDIS_H_
+
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <stdio.h>
+#include	"json.hpp"
+#include    "str.h"
+
+using JSON = nlohmann::json;
+#include    <hiredis-1.1.0/hiredis.h>
+
+
+class Redis
+{
+public:
+
+    Redis() {}
+
+    ~Redis()
+    {
+        this->_connect = NULL;
+        this->_reply = NULL;
+    }
+
+    bool connect(std::string host, int port)
+    {
+        this->_connect = redisConnect(host.c_str(), port);
+        if (this->_connect != NULL && this->_connect->err)
+        {
+            printf("connect error: %s\n", this->_connect->errstr);
+            return 0;
+        }
+        return 1;
+    }
+
+    std::vector<redis_phone>    getAll_noServed(std::string token,int cursor = 0) {
+        std::vector<redis_phone> ret;
+        redisReply* reply = (redisReply*)redisCommand(this->_connect, "HGETALL phone");
+        if (reply == NULL) {
+            std::cout << "Failed to execute Redis command" << std::endl;
+            return ret;
+        }
+        if (reply->type == REDIS_REPLY_ARRAY) {
+            for (int i = 0; i < reply->elements; i += 2) {
+                JSON    data = JSON::parse(reply->element[i + 1]->str);
+                for (int j = 0; j < data["Served"].size(); j++) {
+                    if (data["Served"][j] == token)
+                        continue;
+                    else {
+                        redis_phone re;
+                        re.real_phone = data["real_phone"];
+                        re.phone = reply->element[i]->str;
+                        ret.push_back(re);
+                    }
+                }
+            }
+        }
+        freeReplyObject(reply);
+        return ret;
+    }
+
+    bool    has_Served(std::string phone, std::string token) {
+        redisReply* reply = (redisReply*)redisCommand(this->_connect, "HGET phone %s", phone.c_str());
+        JSON    data = JSON::parse(reply->str);
+        freeReplyObject(reply);
+        for (int i = 0; i < data["Served"].size(); i++) {
+            if (data["Served"][i] == token)
+                return  true;
+        }
+        return  false;
+    }
+    void    add_phone_Served(std::string phone, std::string token) {
+        redisReply* reply = (redisReply*)redisCommand(this->_connect, "HGET phone %s", phone.c_str());
+        JSON    data= JSON::parse(reply->str);
+        data["Served"].push_back(token);
+        freeReplyObject(reply);
+        reply = (redisReply*)redisCommand(this->_connect, "HSET phone %s %s", phone.c_str(), data.dump().c_str());
+        freeReplyObject(reply);
+    }
+
+    void    add_phone(std::string phone,std::string real_phone) {
+        JSON    data ;
+        data["Served"] = JSON::array();
+        data["real_phone"] = real_phone;
+        redisReply* reply = (redisReply*)redisCommand(this->_connect, "HSET phone %s %s", phone.c_str(), data.dump().c_str());
+        freeReplyObject(reply);
+    }
+    bool    exist_phone(std::string phone) {
+        redisReply* reply = (redisReply*)redisCommand(this->_connect, "HEXISTS phone %s", phone.c_str());
+        int ret = reply->integer;
+        freeReplyObject(reply);
+        return  ret;
+    }
+    bool    exist_user(std::string token) {
+       redisReply* reply = (redisReply*)redisCommand(this->_connect, "SISMEMBER %s %s", user_db_name.c_str(),token.c_str());
+       int ret = reply->integer;
+       freeReplyObject(reply);
+       return ret;
+    }
+    void    add_user(std::string user) {
+        redisCommand(this->_connect, "sadd %s %s",  user_db_name.c_str(),user.c_str());
+    }
+
+private:
+    std::string user_db_name = "users";
+    redisContext* _connect;
+    redisReply* _reply;
+};
+
+#endif 
